9967f5d143b53a9d773bd71065fb1dbc482c5f39
Olga Andro <9300510@gmail.com>
2023-09-03 14:21:09 +0300

    Решена задача https://www.codewars.com/kata/555a67db74814aa4ee0001b5/solutions/java

---
+++ src/Is_it_even.java
@@ -0,0 +1,17 @@
+public class Is_it_even {
+    /*
+    In this Kata we are passing a number (n) into a function.
+Your code will determine if the number passed is even (or not).
+The function needs to return either a true or false.
+Numbers may be positive or negative, integers or floats.
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+     */
+    public boolean isEven(double n) {
+        // Your awesome code here!
+        return n % 2 == 0;
+    }
+
+
+
+
+}
